bea9de78102c78b360136e6fc28ba4cccfb1c521
silviotista93 <silviotista93@gmail.com>
2018-11-13 16:59:15 -0500

    images_1

--- app/ProjectImage.php
+++ app/ProjectImage.php
@@ -24,6 +24,6 @@ class ProjectImage extends Model
     protected $guarded = [];
 
     public function pathAttachment(){
-        return '/images/projectsimages/'. $this->url;
+        return '/images/projects/'. $this->url;
     }
 }
--- database/factories/ProjectImageFactory.php
+++ database/factories/ProjectImageFactory.php
@@ -6,6 +6,6 @@ $factory->define(App\ProjectImage::class, function (Faker $faker) {
 
     return [
         'project_id' => \App\Project::all()->random()->id,
-        'url' => \Faker\Provider\Image::image(storage_path() . '/app/public/projectsimages',670,460,'fashion',false)
+        'url' => \Faker\Provider\Image::image(storage_path() . '/app/public/projects',670,460,'fashion',false)
     ];
 });
