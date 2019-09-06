3c2a6bf588c7a706345ff2658d1db68ebf486f04
casalazar562 <casalazar@unimayor.edu.co>
2019-09-06 12:33:14 -0500

    dashboard admin

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -17,12 +17,18 @@ class DashboardController extends Controller
         $last_rejected = Project::where('status',Project::REJECTED)->with('artists')->take(2)->get();
         $last_revisions = Project::where('status',Project::REVISION)->with('artists')->take(2)->get();
         $last_pre_approved = Project::where('status',Project::PREAPPROVAL)->with('artists')->take(2)->get();
+        // numero de proyectos por estado
+        $published = Project::where('status', 4)->with('artists')->count();
+        $rejected = Project::where('status', 5)->with('artists')->count();
+        $approved = Project::where('status', 3)->with('artists')->count();
+        $pre_approved = Project::where('status', 2)->with('artists')->count();
 
         //ULTIMOS ARTISTAS REGISTRADOS
         $last_artists = Artist::with('users','location','countries')->latest()->take(5)->get();
 
         return view('backend.dashboard.dashboard',compact(
             'last_published','last_rejected','last_approved',
-            'last_revisions','last_pre_approved','last_artists'));
+            'last_revisions','last_pre_approved','last_artists', 
+            'published', 'rejected','approved', 'pre_approved'));
     }
 }
